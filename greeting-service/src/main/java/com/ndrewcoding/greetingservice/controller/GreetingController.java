4489ec532e100eab84a74fb0f319b097fc655252
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-14 12:12:03 -0300

    Feat: update the implementation of '/greeting' endpoint to integrate it with the existing custom configuration

--- greeting-service/src/main/java/com/ndrewcoding/greetingservice/controller/GreetingController.java
+++ greeting-service/src/main/java/com/ndrewcoding/greetingservice/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.ndrewcoding.greetingservice.controller;
 
+import com.ndrewcoding.greetingservice.configuration.GreetingConfiguration;
 import com.ndrewcoding.greetingservice.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,12 +11,21 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s";
+    private static final String template = "%s, %s";
     private final AtomicLong counter = new AtomicLong();
+
+    private final GreetingConfiguration configuration;
+
+    public GreetingController(GreetingConfiguration configuration) {
+        this.configuration = configuration;
+    }
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+        if (name.isEmpty()) name = configuration.getDefaultValue();
+
+        return new Greeting(counter.incrementAndGet(), String.format(template, configuration.getGreeting(), name));
     }
 
 }
