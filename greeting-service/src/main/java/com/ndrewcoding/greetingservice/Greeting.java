a2aabbbe7e609ae9b0cb4acf68a0f4312d4e5b38
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-14 11:37:32 -0300

    Feat: add basic fields for Greeting class

--- greeting-service/src/main/java/com/ndrewcoding/greetingservice/Greeting.java
+++ greeting-service/src/main/java/com/ndrewcoding/greetingservice/Greeting.java
@@ -1,4 +1,21 @@
 package com.ndrewcoding.greetingservice;
 
 public class Greeting {
+
+    private final long id;
+    private final String name;
+
+    public Greeting(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
