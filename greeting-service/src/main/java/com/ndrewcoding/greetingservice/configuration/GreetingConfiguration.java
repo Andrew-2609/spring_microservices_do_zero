4603f1f3f35f0e1d4b4e1af46aab6a18de4e1e02
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-14 12:11:08 -0300

    Feat: define a configuration file to handle Greetings

--- greeting-service/src/main/java/com/ndrewcoding/greetingservice/configuration/GreetingConfiguration.java
+++ greeting-service/src/main/java/com/ndrewcoding/greetingservice/configuration/GreetingConfiguration.java
@@ -1,4 +1,33 @@
 package com.ndrewcoding.greetingservice.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
+
+    private String greeting;
+    private String defaultValue;
+
+    public GreetingConfiguration() {
+
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
 }
