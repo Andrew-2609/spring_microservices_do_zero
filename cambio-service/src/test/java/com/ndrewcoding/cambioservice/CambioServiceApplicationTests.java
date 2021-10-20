185e0602e5ec69340c34750499478c501f6255aa
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-10-20 00:46:59 -0300

    Feat: create the project cambio-service

---
+++ cambio-service/src/test/java/com/ndrewcoding/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndrewcoding.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
