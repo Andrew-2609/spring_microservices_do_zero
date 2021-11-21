4167346f39b2048d36724d2b47b90013b59ca672
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-21 17:57:27 -0300

    Feat: add basic annotations for CambioController.java

--- cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
@@ -1,4 +1,10 @@
 package com.ndrewcoding.cambioservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cambio-service")
 public class CambioController {
+
 }
