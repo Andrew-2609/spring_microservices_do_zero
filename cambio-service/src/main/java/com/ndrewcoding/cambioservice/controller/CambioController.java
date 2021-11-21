f74354854fd1d4141e9ec42c9ea06b410d4bea37
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-21 18:02:53 -0300

    Feat: add basic mocked endpoint to return a new Cambio

--- cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
@@ -1,10 +1,20 @@
 package com.ndrewcoding.cambioservice.controller;
 
+import com.ndrewcoding.cambioservice.model.Cambio;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
+
+    @GetMapping("/{amount}/{from}/{to}")
+    public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
+        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+    }
 
 }
