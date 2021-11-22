4f49dc0d8b30e89eeb3cfb6e85629d27e546920f
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-21 22:37:46 -0300

    Feat: add the environment dynamically in the GET request

--- cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
@@ -1,6 +1,7 @@
 package com.ndrewcoding.cambioservice.controller;
 
 import com.ndrewcoding.cambioservice.model.Cambio;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,17 @@ import java.math.BigDecimal;
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    private final Environment environment;
+
+    public CambioController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+        String port = environment.getProperty("local.server.port");
+
+        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
     }
 
 }
