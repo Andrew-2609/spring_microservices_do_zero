f3a64c97247578ecd7ca7ede9afac4ef4c55bc36
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-23 09:47:23 -0300

    Build: add default annotations to make BookRepository a JpaRepository

--- book-service/src/main/java/com/ndrewcoding/bookservice/repository/BookRepository.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/repository/BookRepository.java
@@ -1,4 +1,9 @@
 package com.ndrewcoding.bookservice.repository;
 
-public interface BookRepository {
+import com.ndrewcoding.bookservice.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
