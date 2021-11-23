6092385c25a5564cd608dfb506b0af9583469c90
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-23 09:17:56 -0300

    Feat: add fields and basic implementation of Book.java

--- book-service/src/main/java/com/ndrewcoding/bookservice/model/Book.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/model/Book.java
@@ -1,4 +1,102 @@
 package com.ndrewcoding.bookservice.model;
 
-public class Book {
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Book implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String author;
+    private String title;
+    private String currency;
+    private String environment;
+    private Date launchDate;
+    private Double price;
+
+    public Book() {
+
+    }
+
+    public Book(Long id, String author, String title, String currency, String environment, Date launchDate, Double price) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.currency = currency;
+        this.environment = environment;
+        this.launchDate = launchDate;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(author, book.author) && Objects.equals(title, book.title) && Objects.equals(currency, book.currency) && Objects.equals(environment, book.environment) && Objects.equals(launchDate, book.launchDate) && Objects.equals(price, book.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, title, currency, environment, launchDate, price);
+    }
 }
