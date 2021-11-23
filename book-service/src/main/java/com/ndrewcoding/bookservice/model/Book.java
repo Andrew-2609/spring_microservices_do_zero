e067fea96507d200665b2028c2b62476f189268d
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-23 09:44:14 -0300

    Build: add javax.persistence annotatinos for Book model

--- book-service/src/main/java/com/ndrewcoding/bookservice/model/Book.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/model/Book.java
@@ -1,20 +1,37 @@
 package com.ndrewcoding.bookservice.model;
 
+import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+@Entity(name = "book")
 public class Book implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 180)
     private String author;
+
+    @Column(nullable = false, length = 250)
     private String title;
+
+    @Column(name = "launch_date", nullable = false)
+    @Temporal(TemporalType.DATE)
     private Date launchDate;
+
+    @Column(nullable = false)
     private Double price;
+
+    @Transient
     private String currency;
+
+    @Transient
     private String environment;
 
     public Book() {
