bb8916a8b534c88b7c111bc8bfb4ff9d6c32f0b3
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-23 08:27:59 -0300

    Feat: implement getCambio endpoint with actual data from MySQL database

--- cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
@@ -1,6 +1,7 @@
 package com.ndrewcoding.cambioservice.controller;
 
 import com.ndrewcoding.cambioservice.model.Cambio;
+import com.ndrewcoding.cambioservice.repository.CambioRepository;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,22 +9,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    private final CambioRepository cambioRepository;
     private final Environment environment;
 
-    public CambioController(Environment environment) {
+    public CambioController(CambioRepository cambioRepository, Environment environment) {
+        this.cambioRepository = cambioRepository;
         this.environment = environment;
     }
 
     @GetMapping("/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
-        String port = environment.getProperty("local.server.port");
+        Cambio cambio = cambioRepository.findByFromAndTo(from, to);
+        if (cambio == null) throw new RuntimeException("Currency Unsupported!");
 
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
+        String port = environment.getProperty("local.server.port");
+        cambio.setEnvironment(port);
+
+        BigDecimal conversionFactor = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+
+        return cambio;
     }
 
 }
