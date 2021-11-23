9d8657bb82e89ce4738df8e8643c4984d59a3afa
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-23 08:01:50 -0300

    Build: add javax.persistence annotations for Cambio model

--- cambio-service/src/main/java/com/ndrewcoding/cambioservice/model/Cambio.java
+++ cambio-service/src/main/java/com/ndrewcoding/cambioservice/model/Cambio.java
@@ -1,19 +1,33 @@
 package com.ndrewcoding.cambioservice.model;
 
+import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+@Entity(name = "cambio")
 public class Cambio implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "from_currency", nullable = false, length = 3)
     private String from;
+
+    @Column(name = "to_currency", nullable = false, length = 3)
     private String to;
+
+    @Column(nullable = false)
     private BigDecimal conversionFactor;
+
+    @Transient
     private BigDecimal convertedValue;
+
+    @Transient
     private String environment;
 
     public Cambio() {
