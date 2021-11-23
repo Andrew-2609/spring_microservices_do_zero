6b71704da215b0462bcc63d8b3a35d9f09e6d98e
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-23 08:05:05 -0300

    Feat: add method to return a Cambio with 'from' and 'to' parameters

--- cambio-service/src/main/java/com/ndrewcoding/cambioservice/repository/CambioRepository.java
+++ cambio-service/src/main/java/com/ndrewcoding/cambioservice/repository/CambioRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CambioRepository extends JpaRepository<Cambio, Long> {
+    Cambio findByFromAndTo(String from, String to);
 }
