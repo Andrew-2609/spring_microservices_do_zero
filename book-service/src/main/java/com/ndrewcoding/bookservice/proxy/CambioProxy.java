8ea26d5f43a32e8d70710b6700541c01333bf664
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-25 14:53:20 -0300

    Feat: add endpoint getCambio in CambioProxy

--- book-service/src/main/java/com/ndrewcoding/bookservice/proxy/CambioProxy.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/proxy/CambioProxy.java
@@ -1,7 +1,16 @@
 package com.ndrewcoding.bookservice.proxy;
 
+import com.ndrewcoding.bookservice.response.Cambio;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
 
 @FeignClient(name = "cambio-service", url = "localhost:8000")
 public interface CambioProxy {
+
+    @GetMapping("/cambio-service/{amount}/{from}/{to}")
+    public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to);
+
 }
