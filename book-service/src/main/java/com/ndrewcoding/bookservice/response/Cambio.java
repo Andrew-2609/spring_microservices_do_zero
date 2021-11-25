9d6675706cfa278e05e6d834310cb591893c8166
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-25 14:43:24 -0300

    Feat: add Cambio.java to be consumed by book-service

---
+++ book-service/src/main/java/com/ndrewcoding/bookservice/response/Cambio.java
@@ -0,0 +1,91 @@
+package com.ndrewcoding.bookservice.response;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Cambio implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String from;
+    private String to;
+    private Double conversionFactor;
+    private Double convertedValue;
+    private String environment;
+
+    public Cambio() {
+
+    }
+
+    public Cambio(Long id, String from, String to, Double conversionFactor, Double convertedValue, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionFactor = conversionFactor;
+        this.convertedValue = convertedValue;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public Double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public void setConversionFactor(Double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public Double getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(Double convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cambio cambio = (Cambio) o;
+        return Objects.equals(id, cambio.id) && Objects.equals(from, cambio.from) && Objects.equals(to, cambio.to) && Objects.equals(conversionFactor, cambio.conversionFactor) && Objects.equals(convertedValue, cambio.convertedValue) && Objects.equals(environment, cambio.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, from, to, conversionFactor, convertedValue, environment);
+    }
+}
