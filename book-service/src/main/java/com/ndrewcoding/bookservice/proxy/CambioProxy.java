ffcbbbdd068bd7c13cc2c41038d799b428b80e0c
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-25 14:51:27 -0300

    Feat: make CambioProxy a FeignClient of cambio-service

--- book-service/src/main/java/com/ndrewcoding/bookservice/proxy/CambioProxy.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/proxy/CambioProxy.java
@@ -1,4 +1,7 @@
 package com.ndrewcoding.bookservice.proxy;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "cambio-service", url = "localhost:8000")
 public interface CambioProxy {
 }
