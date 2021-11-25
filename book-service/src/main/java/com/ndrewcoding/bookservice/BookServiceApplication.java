100957dd06ad6a95b0d58d218399e316643eec28
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-25 14:49:23 -0300

    Feat: add @EnableFeignClients annotation to BookServiceApplication.java

--- book-service/src/main/java/com/ndrewcoding/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/BookServiceApplication.java
@@ -2,8 +2,10 @@ package com.ndrewcoding.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BookServiceApplication {
 
     public static void main(String[] args) {
