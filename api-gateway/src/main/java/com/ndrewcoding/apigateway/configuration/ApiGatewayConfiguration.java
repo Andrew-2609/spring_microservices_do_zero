446a7e0299e5b22965b31c05de877f1e2a6a543c
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-29 17:55:29 -0300

    Feat: add basic setup for ApiGatewayConfiguration

--- api-gateway/src/main/java/com/ndrewcoding/apigateway/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/ndrewcoding/apigateway/configuration/ApiGatewayConfiguration.java
@@ -1,4 +1,16 @@
 package com.ndrewcoding.apigateway.configuration;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes().build();
+    }
+
 }
