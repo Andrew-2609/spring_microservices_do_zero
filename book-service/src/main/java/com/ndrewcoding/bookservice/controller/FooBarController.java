079a7e6be3b74ae0a009d384a8526b6cccfd13ea
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-30 07:46:15 -0300

    Feat: add logging and @Retry annotation to FooBarController endpoint

--- book-service/src/main/java/com/ndrewcoding/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/controller/FooBarController.java
@@ -1,5 +1,9 @@
 package com.ndrewcoding.bookservice.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +13,14 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("book-service")
 public class FooBarController {
 
+    private final Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
     @GetMapping("/foo-bar")
+    @Retry(name = "default")
     public String fooBar() {
-        new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
-        return "Foo-Bar";
+        logger.info("Request to foo-bar was received!");
+        ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+        return response.getBody();
     }
 
 }
