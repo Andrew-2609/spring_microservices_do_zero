b90c2622c21a37ffd512f8bc51882a90911dec21
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-29 21:22:44 -0300

    Feat: add routes to redirect services to the Load Balancer in api-gateway

--- api-gateway/src/main/java/com/ndrewcoding/apigateway/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/ndrewcoding/apigateway/configuration/ApiGatewayConfiguration.java
@@ -19,6 +19,8 @@ public class ApiGatewayConfiguration {
                                 )
                                 .uri("http://httpbin.org:80")
                 )
+                .route(p -> p.path("/cambio-service/**").uri("lb://cambio-service"))
+                .route(p -> p.path("/book-service/**").uri("lb://book-service"))
                 .build();
     }
 
