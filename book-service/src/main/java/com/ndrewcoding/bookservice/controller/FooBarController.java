d2aa2b6467a59fa98f932872434a18c10b991a5c
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-30 08:16:09 -0300

    Feat: replace CircuitBreaker with RateLimiter in FooBarController

--- book-service/src/main/java/com/ndrewcoding/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/controller/FooBarController.java
@@ -1,13 +1,11 @@
 package com.ndrewcoding.bookservice.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("book-service")
@@ -16,11 +14,11 @@ public class FooBarController {
     private final Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    @RateLimiter(name = "default", fallbackMethod = "fallbackMethod")
     public String fooBar() {
         logger.info("Request to foo-bar was received!");
-        ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
-        return response.getBody();
+        // ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+        return "Foo-Bar!!!";
     }
 
     public String fallbackMethod(Exception exception) {
