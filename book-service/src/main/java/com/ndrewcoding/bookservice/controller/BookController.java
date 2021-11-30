97e8d1f435bf5b294a8a224084fed7746acd3ecf
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-30 08:57:07 -0300

    Docs: add basic Swagger documentation for BookController

--- book-service/src/main/java/com/ndrewcoding/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/controller/BookController.java
@@ -4,12 +4,15 @@ import com.ndrewcoding.bookservice.model.Book;
 import com.ndrewcoding.bookservice.proxy.CambioProxy;
 import com.ndrewcoding.bookservice.repository.BookRepository;
 import com.ndrewcoding.bookservice.response.Cambio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book Endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -24,6 +27,7 @@ public class BookController {
         this.environment = environment;
     }
 
+    @Operation(summary = "Returns a specific book by its ID")
     @GetMapping("/{id}/{currency}")
     public Book getBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
         Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not Found!"));
