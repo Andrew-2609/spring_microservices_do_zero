6a223a8aa2511c127a43fd5f3662715092158741
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-30 08:50:54 -0300

    Feat: add OpenAPI Definition basic information to book-service

--- book-service/src/main/java/com/ndrewcoding/bookservice/configuration/OpenApiConfiguration.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/configuration/OpenApiConfiguration.java
@@ -1,4 +1,8 @@
 package com.ndrewcoding.bookservice.configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Book Service API", version = "v1", description = "Documentation of Book Service API"))
 public class OpenApiConfiguration {
 }
