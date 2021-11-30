d38f4893700bb5a96a435f324afb19140645c813
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-30 07:42:29 -0300

    Feat: add problematic logic to FooBarController

--- book-service/src/main/java/com/ndrewcoding/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/ndrewcoding/bookservice/controller/FooBarController.java
@@ -1,4 +1,18 @@
 package com.ndrewcoding.bookservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("book-service")
 public class FooBarController {
+
+    @GetMapping("/foo-bar")
+    public String fooBar() {
+        new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+        return "Foo-Bar";
+    }
+
 }
