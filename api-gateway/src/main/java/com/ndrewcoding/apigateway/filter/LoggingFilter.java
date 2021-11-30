79a90a21ebb1efb6a7dfce1526357fb43d036e20
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-29 21:49:05 -0300

    Feat: add an effective Logger to LoggingFilter to show the original request path

--- api-gateway/src/main/java/com/ndrewcoding/apigateway/filter/LoggingFilter.java
+++ api-gateway/src/main/java/com/ndrewcoding/apigateway/filter/LoggingFilter.java
@@ -1,15 +1,22 @@
 package com.ndrewcoding.apigateway.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class LoggingFilter implements GlobalFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+        logger.info("Original request path: {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
     }
 
 }
