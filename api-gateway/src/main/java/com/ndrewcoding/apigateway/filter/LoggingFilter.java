528860dde07e36634bc2fd30d497bf735d4afa37
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-11-29 21:46:00 -0300

    Feat: add default implementation for GlobalFilter interface

--- api-gateway/src/main/java/com/ndrewcoding/apigateway/filter/LoggingFilter.java
+++ api-gateway/src/main/java/com/ndrewcoding/apigateway/filter/LoggingFilter.java
@@ -1,4 +1,15 @@
 package com.ndrewcoding.apigateway.filter;
 
-public class LoggingFilter {
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class LoggingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+
 }
