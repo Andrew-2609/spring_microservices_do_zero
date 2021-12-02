14f03e1484e21efdf37e29d7f18029b919a2c4ca
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-01 21:59:52 -0300

    Feat: add OpenApiConfiguration to cambio-service

---
+++ cambio-service/src/main/java/com/ndrewcoding/cambioservice/configuration/OpenApiConfiguration.java
@@ -0,0 +1,29 @@
+package com.ndrewcoding.cambioservice.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+
+@OpenAPIDefinition(info = @Info(title = "Cambio Service API", version = "v1", description = "Documentation of Cambio Service API"))
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(
+                        new io.swagger.v3.oas.models.info.Info()
+                                .title("Cambio Service API")
+                                .version("v1")
+                                .license(
+                                        new License()
+                                                .name("Apache 2.0")
+                                                .url("http://springdoc.org")
+                                )
+                );
+    }
+
+}
