00e348b77961300f23c69f61b66507bdd5270231
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-01 22:41:58 -0300

    Feat: add implementation of OpenApiConfiguration with route definitions for the APIs

--- api-gateway/src/main/java/com/ndrewcoding/apigateway/configuration/OpenApiConfiguration.java
+++ api-gateway/src/main/java/com/ndrewcoding/apigateway/configuration/OpenApiConfiguration.java
@@ -1,7 +1,12 @@
 package com.ndrewcoding.apigateway.configuration;
 
 import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +14,20 @@ import java.util.List;
 @Configuration
 public class OpenApiConfiguration {
 
-    public List<GroupedOpenApi> apis() {
+    @Bean
+    @Lazy(value = false)
+    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters configParameters, RouteDefinitionLocator locator) {
+        List<RouteDefinition> routeDefinitions = locator.getRouteDefinitions().collectList().block();
+
+        routeDefinitions.stream()
+                .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
+                .forEach(routeDefinition -> {
+                    String name = routeDefinition.getId();
+                    configParameters.addGroup(name);
+                    GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+                })
+        ;
+
         return new ArrayList<>();
     }
 
