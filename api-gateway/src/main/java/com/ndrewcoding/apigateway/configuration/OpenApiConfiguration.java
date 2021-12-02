36ab3a8b69953f2c4082e34953d23679fcf61644
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-01 22:31:02 -0300

    Feat: add basic implementation of OpenApiConfiguration class

--- api-gateway/src/main/java/com/ndrewcoding/apigateway/configuration/OpenApiConfiguration.java
+++ api-gateway/src/main/java/com/ndrewcoding/apigateway/configuration/OpenApiConfiguration.java
@@ -1,7 +1,16 @@
 package com.ndrewcoding.apigateway.configuration;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfiguration {
+
+    public List<GroupedOpenApi> apis() {
+        return new ArrayList<>();
+    }
+
 }
