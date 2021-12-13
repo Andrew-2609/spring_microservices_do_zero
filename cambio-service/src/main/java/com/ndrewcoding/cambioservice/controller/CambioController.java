66e8afe3ae69a87a3720ad419a208d3151648855
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-12 23:45:08 -0300

    Chore: add simple Logger to CambioController.getCambio

--- cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/ndrewcoding/cambioservice/controller/CambioController.java
@@ -4,6 +4,8 @@ import com.ndrewcoding.cambioservice.model.Cambio;
 import com.ndrewcoding.cambioservice.repository.CambioRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,8 @@ import java.math.RoundingMode;
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    private final Logger logger = LoggerFactory.getLogger(CambioController.class);
+
     private final CambioRepository cambioRepository;
     private final Environment environment;
 
@@ -29,6 +33,8 @@ public class CambioController {
     @Operation(summary = "Returns the exchange from a given currency")
     @GetMapping("/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
+        logger.info("getCambio was called with -> amount: {}, from: {}, to: {}", amount, from, to);
+
         Cambio cambio = cambioRepository.findByFromAndTo(from, to);
         if (cambio == null) throw new RuntimeException("Currency Unsupported!");
 
